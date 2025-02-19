a6e5f7e332465122bebe1fb34f4288e43bf12d78
Austin Christian Adithia <austinandadithia@gmail.com>
2025-02-18 20:30:23 -0800

    second commit

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 print("Hello World!")
 print("I'm Austin and I'm currently 20 years old.")
-
+print("I had a crush on an Indonesian student in Shoreline CC.")
+print("I think she's the most beautiful person in the world with her gorgeous eyes and long-flowing hair")
+print("Unfortunately, I was heartbroken when I learned that she had a boyfriend.")
+print("End of story.")
