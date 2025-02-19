b14ce5328a63629200f57fa34637275b3e2a9c45
Austin Christian Adithia <austinandadithia@gmail.com>
2025-02-18 23:43:01 -0800

    third commit

---
+++ bruh.py
@@ -0,0 +1,2 @@
+print("I'm having a bruh moment right now.")
+print("2 + 2 = 4")
--- test.py
+++ test.py
@@ -4,3 +4,4 @@ print("I had a crush on an Indonesian student in Shoreline CC.")
 print("I think she's the most beautiful person in the world with her gorgeous eyes and long-flowing hair")
 print("Unfortunately, I was heartbroken when I learned that she had a boyfriend.")
 print("End of story.")
+
